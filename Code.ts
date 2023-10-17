38564ffc39bed00afefc2613ea384f4c519b2af6
Théo Boulaire <th.boulaire@gmail.com>
2023-10-17 13:39:39 +0200

    Review tasks handling script init

--- Code.ts
+++ Code.ts
@@ -1,3 +1,21 @@
 function myFunction() {
-  Logger.log('Hello world !');
+  const taskLists = Tasks.Tasklists?.list().items ?? [];
+  // const reviewListTitle = "Bilans";
+  const reviewListTitle = "Bilans";
+  let reviewListId: string | undefined;
+  for (const { title, id } of taskLists) {
+    Logger.log(`${title} - ${id}`);
+    if (title === reviewListTitle) {
+      reviewListId = id;
+    }
+  }
+  if (reviewListId === undefined) {
+    return;
+  }
+  const reviewTasks = Tasks.Tasks?.list(reviewListId, { showCompleted: true, showHidden: true }).items ?? [];
+  Logger.log(reviewTasks.length);
+  for (const { title, id, completed } of reviewTasks) {
+    Logger.log(`${title} - ${id} - ${completed}`);
+    //Tasks.Tasks?.patch({ notes: 'hello world' }, reviewListId, id as string);
+  }
 }
