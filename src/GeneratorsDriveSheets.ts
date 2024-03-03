c6038d65882eb08ed08c726f073819cb28e45f72
Théo Boulaire <th.boulaire@gmail.com>
2024-03-03 08:36:40 +0100

    Script properties

--- src/GeneratorsDriveSheets.ts
+++ src/GeneratorsDriveSheets.ts
@@ -5,14 +5,15 @@ declare const DriveSheets: DriveSheetsLibrary;
 namespace GeneratorsDriveSheets {
   type Line = [string, string, string, string, string];
 
-  const fileName = "Tasks Generators";
-  const range = "Feuille 1!A2:E";
+  const scriptProperties = PropertiesService.getScriptProperties();
+  const fileName = scriptProperties.getProperty("generatorsFileName") as string;
+  const range = scriptProperties.getProperty("generatorsRange") as string;
 
   export const load = () => {
     return DriveSheets.load({ fileName, range }) as Line[];
   }
 
   export const save = (values: Line[]) => {
-    return DriveSheets.save({ fileName, values, range });
+    return DriveSheets.save({ fileName, range, values });
   }
 }
